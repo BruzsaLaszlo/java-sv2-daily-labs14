345a9fa98cb9f1b740698dd15016d3991f0b17bd
Bruzsa László <lacika007@gmail.com>
2022-01-27 15:43:16 +0100

    day04

---
+++ src/main/java/day04/PairFinder.java
@@ -0,0 +1,20 @@
+package day04;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class PairFinder {
+
+    public long findPairs(List<Integer> numbers) {
+        return numbers.stream()
+                .collect(groupingBy(
+                        o -> o,
+                        counting()))
+                .values().stream()
+                .mapToLong(aLong -> aLong / 2)
+                .sum();
+    }
+
+}
---
+++ src/test/java/day04/PairFinderTest.java
@@ -0,0 +1,21 @@
+package day04;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairFinderTest {
+
+        PairFinder pf = new PairFinder();
+
+    @Test
+    void findPairs() {
+        var numbers = List.of(5, 1, 4, 5);
+        assertEquals(1,pf.findPairs(numbers));
+
+        numbers = List.of(7, 1, 5, 7, 3, 3, 5, 7, 6, 7);
+        assertEquals(4,pf.findPairs(numbers));
+    }
+}
