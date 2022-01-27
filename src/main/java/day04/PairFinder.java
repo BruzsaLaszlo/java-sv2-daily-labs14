e91082344819ee16f7574b239c8b81be3a9a9344
Bruzsa László <lacika007@gmail.com>
2022-01-27 16:18:52 +0100

    day04

--- src/main/java/day04/PairFinder.java
+++ src/main/java/day04/PairFinder.java
@@ -1,7 +1,9 @@
 package day04;
 
+import java.util.Comparator;
 import java.util.List;
 
+import static java.util.Comparator.*;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
@@ -15,6 +17,19 @@ public class PairFinder {
                 .values().stream()
                 .mapToLong(count -> count / 2)
                 .sum();
+    }
+
+    public int findPairs(int[] arr) {
+        int[] counts = new int[10];
+        for (int i : arr) {
+            counts[i]++;
+        }
+
+        int result = 0;
+        for (int i = 0; i < counts.length; i++) {
+            result += counts[i] / 2;
+        }
+        return result;
     }
 
 }
--- src/test/java/day04/PairFinderTest.java
+++ src/test/java/day04/PairFinderTest.java
@@ -4,18 +4,27 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PairFinderTest {
 
-        PairFinder pf = new PairFinder();
+    PairFinder pf = new PairFinder();
 
     @Test
     void findPairs() {
         var numbers = List.of(5, 1, 4, 5);
-        assertEquals(1,pf.findPairs(numbers));
+        assertEquals(1, pf.findPairs(numbers));
 
         numbers = List.of(7, 1, 5, 7, 3, 3, 5, 7, 6, 7);
-        assertEquals(4,pf.findPairs(numbers));
+        assertEquals(4, pf.findPairs(numbers));
+    }
+
+    @Test
+    void findPairsArray() {
+        int[] numbers = {5, 1, 4, 5};
+        assertEquals(1, pf.findPairs(numbers));
+
+        numbers = new int[]{7, 1, 5, 7, 3, 3, 5, 7, 6, 7};
+        assertEquals(4, pf.findPairs(numbers));
     }
 }
